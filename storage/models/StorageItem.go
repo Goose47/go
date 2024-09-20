0e45278e77d5749f33df1f5e8945e8a354f46644
Nikita <tyumin000@gmail.com>
2024-09-20 12:20:24 +0300

    feat: add fullPath method

--- storage/models/StorageItem.go
+++ storage/models/StorageItem.go
@@ -1,8 +1,17 @@
 package models
 
+import (
+	"Goose47/storage/config"
+	"path"
+)
+
 type StorageItem struct {
 	Key          string
 	Ttl          int
 	Path         string
 	OriginalName string
+}
+
+func (i *StorageItem) GetFullPath() string {
+	return path.Join(config.FSConfig.Base, i.Path)
 }
